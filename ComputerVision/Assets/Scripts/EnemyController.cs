0e36a9f7330b96873fff9f280ffb0fa8367a8696
Claudiu <doroptg@gmail.com>
2020-08-07 10:24:40 +0300

    The enemy follows the player

--- ComputerVision/Assets/Scripts/EnemyController.cs
+++ ComputerVision/Assets/Scripts/EnemyController.cs
@@ -12,11 +12,11 @@ public class EnemyController : MonoBehaviour
     public GameObject player;
     public Transform playerMovePoint;
 
-    //1 - dreapta, 2 - stanga, 3 - sus, 4 - jos, altceva - sta pe loc
+    // 1 - dreapta, 2 - stanga, 3 - sus, 4 - jos, altceva - sta pe loc -> schimbat
+    // 0 - hold, 1 - up, 2 - down, 3 - left, 4 - right
     public int[] movement;
 
     private int contor = 0;
-
 
     // Start is called before the first frame update
     void Start()
@@ -33,42 +33,69 @@ public class EnemyController : MonoBehaviour
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f || Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                int move = Random.Range(1, 5);
+                /*int move = Random.Range(1, 5);
                 if(contor < movement.Length)
                 {
                     move = movement[contor];
+                }*/
+
+                int move = -1;
+
+                GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+                Vector3 playerPosition = playerObject.transform.position;
+                Vector3 enemyPosition = transform.position;
+
+                float x_dif = enemyPosition.x - playerPosition.x;
+                float y_dif = enemyPosition.y - playerPosition.y;
+
+                if (y_dif <= -0.5)
+                    move = 1;
+                else if (y_dif <= 0.5)
+                {
+                    if (x_dif <= -0.5)
+                        move = 4;
+                    else if (x_dif <= 0.5)
+                        move = 0;
+                    else
+                        move = 3;
                 }
+                else
+                    move = 2;
 
                 switch (move)
                 {
-                    case 1:
-                        if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(1f, 0f, 0f), .2f, whatStopsMovement))
-                        {
-                            movePoint.position += new Vector3(1f, 0f, 0f);
-                        }
+                    case 0: // hold
+                        // the player dies or the enemy dies
                         break;
-                    case 2:
-                        if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(-1f, 0f, 0f), .2f, whatStopsMovement))
-                        {
-                            movePoint.position += new Vector3(-1f, 0f, 0f);
-                        }
-                        break;
-                    case 3:
+                    case 1: // up
                         if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, 1f, 0f), .2f, whatStopsMovement))
                         {
                             movePoint.position += new Vector3(0f, 1f, 0f);
                         }
                         break;
-                    case 4:
+                    case 2: // down
                         if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .2f, whatStopsMovement))
                         {
                             movePoint.position += new Vector3(0f, -1f, 0f);
                         }
                         break;
-                    default:
+                    case 3: // left
+                        if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(-1f, 0f, 0f), .2f, whatStopsMovement))
+                        {
+                            movePoint.position += new Vector3(-1f, 0f, 0f);
+                        }
+                        break;
+                    case 4: // right
+                        if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(1f, 0f, 0f), .2f, whatStopsMovement))
+                        {
+                            movePoint.position += new Vector3(1f, 0f, 0f);
+                        }
+                        break;
+                    default: // error
                         break;
                 }
-                contor++;
+
+                //contor++;
             }
         }
     }
