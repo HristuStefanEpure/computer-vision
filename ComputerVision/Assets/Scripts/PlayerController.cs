1b7aaf2bfc27f4d420ef39487ffec875c9d82c5f
HristuStefanEpure <Caciulacdlac@gmail.com>
2020-08-11 21:41:18 +0300

    Animatie jucator

--- ComputerVision/Assets/Scripts/PlayerController.cs
+++ ComputerVision/Assets/Scripts/PlayerController.cs
@@ -16,19 +16,24 @@ public class PlayerController : MonoBehaviour
     public Text text;
 
     private int score = 0;
+    private int last_move; //0-side, 1-up, 2-down
 
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;
+        last_move = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 scale = transform.localScale;
+
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
 
             //Contorizare scor
@@ -43,6 +48,18 @@ public class PlayerController : MonoBehaviour
 
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
+
+                last_move = 0;
+
+                if (Input.GetAxisRaw("Horizontal") == -1)
+                {
+                    scale.x = 4;
+                }
+                else
+                {
+                    scale.x = -4;
+                }
+
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
@@ -50,10 +67,40 @@ public class PlayerController : MonoBehaviour
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
+                if (Input.GetAxisRaw("Vertical") == 1)
+                {
+                    last_move = 1;
+                }
+                else
+                {
+                    last_move = 2;
+                }
+
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                 }
+            }
+            else
+            {
+                GetComponent<Animator>().SetTrigger("idle");
+            }
+
+            transform.localScale = scale;
+        }
+        else
+        {
+            if (last_move == 0) 
+            { 
+                GetComponent<Animator>().SetTrigger("move_side");
+            }
+            else if (last_move == 1)
+            {
+                GetComponent<Animator>().SetTrigger("move_up");
+            }
+            else
+            {
+                GetComponent<Animator>().SetTrigger("move_down");
             }
         }
 
